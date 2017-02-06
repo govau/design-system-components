21d7d106cb6d329287f0cc1250e7fe1d1f561050
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-02-07 09:45:34 +1100

    working on #74, Added circle integration
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- scripts/cideploy.sh
+++ scripts/cideploy.sh
@@ -3,10 +3,21 @@
 # Exit immediately if any commands return non-zero
 set -e
 
-# download cf-cli and use to push
+# download cf-cli
 curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
 sudo dpkg -i cf-cli_amd64.deb
 cf -v
-cf login -a https://api.system.staging.digital.gov.au -o guides -s uikit -u $CF_USER_STAGING -p $CF_PASSWORD_STAGING
-cf target -o guides -s uikit
-# cf push gov-au-ui-kit$1 -b staticfile_buildpack -p ./build -i 1
+
+# check for staging flag
+if [[ $* == *--staging* ]]
+then
+	# push with staging vars
+	cf login -a https://api.system.staging.digital.gov.au -o $CF_ORG_STAGING -s $CF_SPACE_STAGING -u $CF_USER_STAGING -p $CF_PASSWORD_STAGING
+	cf target -o $CF_ORG_STAGING -s $CF_SPACE_STAGING
+	cf push
+else
+	# push with prod vars
+	cf login -a https://api.system.staging.digital.gov.au -o $CF_ORG_PROD -s $CF_SPACE_PROD -u $CF_USER_PROD -p $CF_PASSWORD_PROD
+	cf target -o $CF_ORG_PROD -s $CF_SPACE_PROD
+	cf push
+fi
