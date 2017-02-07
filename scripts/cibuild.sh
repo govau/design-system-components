10c58ee1e55505c846b93155cffd8a3fe0a93246
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-02-07 14:00:07 +1100

    lerna is killing me
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,20 +3,20 @@
 # Fail fast and be aware of exit codes
 set -eo pipefail
 
-echo "\033[1;34mListing current files\033[0m"
+echo "==== Listing current files ===="
 ls -lhaG
 
-echo "\033[1;34mRemoving node_modules folder\033[0m"
+echo "==== Removing node_modules folder ===="
 rm -rf node_modules
 
-echo "\033[1;34mInstalling lerna globally\033[0m"
+echo "==== Installing lerna globally ===="
 npm install lerna@2.0.0-beta.36 -g
 
-echo "\033[1;34mInstalling npm modules\033[0m"
+echo "==== Installing npm modules ===="
 npm install
 
-echo "\033[1;34mRebuilding node-sass because ... magic\033[0m"
+echo "==== Rebuilding node-sass because ... magic ===="
 npm rebuild node-sass
 
-echo "\033[1;34mFinally run build\033[0m"
+echo "==== Finally run build ===="
 npm run build
