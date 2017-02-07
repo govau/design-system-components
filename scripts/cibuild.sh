2f2d8322fd930839c835400737ad47f6479b94f6
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-02-07 12:38:57 +1100

    added some more comments for better ci output
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,10 +3,20 @@
 # Fail fast and be aware of exit codes
 set -eo pipefail
 
-ls
+echo "Listing current files"
+ls -lhaG
+
+echo "\nRemoving node_modules folder"
 rm -rf node_modules
-ls
+
+echo "\nInstalling lerna globally"
 npm install lerna@2.0.0-beta.36 -g
+
+echo "\nInstalling npm modules"
 npm install
+
+echo "\nRebuilding node-sass because ... magic"
 npm rebuild node-sass
+
+echo "\nFinally run build"
 npm run build
