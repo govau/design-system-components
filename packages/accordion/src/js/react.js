ba3f0fb3315668ec777beceea1c369f151b12053
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-05-19 11:29:49 +1000

    working on accordion react component
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- packages/accordion/src/js/react.js
+++ packages/accordion/src/js/react.js
@@ -9,19 +9,27 @@
 
 import React from 'react';
 
-const Accordion = () => (
-	<div className="accordion">
-		<h1>I&rsquo;m an accordion</h1>
-	</div>
-);
+// const Accordion = () => (
+// 	<div className="accordion">
+// 		<h1>I&rsquo;m an accordion</h1>
+// 	</div>
+// );
 
-export default Accordion;
+
+
+
+// export default Accordion;
+
+
+
+
+
 
 // import "accodrdion.scss"
-//
+
 // export const Accordion = () => (
-//
-//
+
+
 // 	<section class="uikit-accordion">
 // 		<a href="#accordion-default"
 // 			class="uikit-accordion__title js-uikit-accordion"
@@ -32,23 +40,23 @@ export default Accordion;
 // 			onclick={props.toggle}>
 // 				Accordion title
 // 		</a>
-//
+
 // 		<div class="uikit-accordion__body" id="accordion-default" aria-hidden="false">
 // 			<div class="uikit-accordion__body-wrapper">
-//
+
 // 				Here <a href="#url">is</a> some accordion content
-//
+
 // 			</div>
 // 		</div>
 // 	</section>
 // )
-//
-//
-//
-//
+
+
+
+
 // import Accordion from '../';
-//
-//
+
+
 // const withOtherStuff = Component => {
 // 	return ({toggle, ...rest}) => {
 // 		const otherStuffToggle = () => {
@@ -58,13 +66,13 @@ export default Accordion;
 //   	return <Component toggle={otherStuffToggle} {...rest} />
 // 	}
 // }
-//
-//
+
+
 // const withToggle = Component => {
-//
+
 // 	let activePaneId = 1;
-//
-//
+
+
 // 	return ({toggle, ...rest}) => {
 // 		const otherStuffToggle = () => {
 // 			console.log('dskjfjdshf');
@@ -73,15 +81,15 @@ export default Accordion;
 //   	return <Component toggle={otherStuffToggle} {...rest} />
 // 	}
 // }
-//
+
 // const MyAccordion = withToggle(Accordion);
-//
+
 // return <MyAccordion />
-//
-//
-//
-//
-//
+
+
+
+
+
 // const HorizAccordion = ({onOpenPane, isOpen}) => {
 // 	return (
 // 		<div>
@@ -91,7 +99,7 @@ export default Accordion;
 // 	)
 // }
 // export  withAccordion(HorizAccordion);
-//
+
 // const VertAccordion = ({onOpenPane, isOpen}) => {
 // 	return (
 // 		<div>
@@ -100,8 +108,8 @@ export default Accordion;
 // 		</div>
 // 	)
 // }
-//
-//
+
+
 // const withAccordion = Component => {
 // 	return class extends React.Component {
 // 		constructor(props) {
@@ -118,3 +126,61 @@ export default Accordion;
 // 		}
 // 	}
 // }
+
+
+
+export class Accordion extends React.PureComponent {
+	componentWillReceiveProps( nextProps ) {
+		const next = nextProps.open;
+
+		if( next ) {
+			UIKIT.accordion.Open( this.accordionHeader );
+		}
+		else {
+			UIKIT.accordion.Close( this.accordionHeader );
+		}
+	}
+
+	toggle( event ) {
+		event.preventDefault();
+
+		return UIKIT.accordion.Toggle( this.accordionHeader );
+	}
+
+	render() {
+		return (
+			<section className="uikit-accordion">
+				<a href="#accordion-default"
+					className={`uikit-accordion__title js-uikit-accordion { !open && uikit-accordion--closed}`}
+					aria-controls="accordion-default"
+					aria-expanded="true"
+					aria-selected="true"
+					role="tab"
+					ref={ accordionHeader => { this.accordionHeader = accordionHeader } }
+					onClick={ event => this.toggle( event ) }>
+						Accordion title
+				</a>
+
+				<div className={`uikit-accordion__body { !open && uikit-accordion--closed}`} id="accordion-default" aria-hidden="false">
+					<div className="uikit-accordion__body-wrapper">
+
+						Here <a href="#url">is</a> some accordion content
+
+					</div>
+				</div>
+			</section>
+		);
+	}
+};
+
+
+
+
+// <Accordian open={this.state.open} onClick={() => this.setState({open: !this.state.open})} header="sdhfgkjdshb">
+// 	sdf;dsjhfkjdsahbf lksdfjhg
+// </Accordian>
+
+
+// <Accordian header="sdhfgkjdshb">
+// 	sdf;dsjhfkjdsahbf lksdfjhg
+// </Accordian>
--- packages/accordion/tests/react/index.js
+++ packages/accordion/tests/react/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Accordion from './react.js';
+import { Accordion } from './react.js';
 
 ReactDOM.render(
-	<Accordion>
-	</Accordion>,
+	<div>
+		<h2>Default accordion</h2>
+
+		<Accordion>
+			Some content of the accordion <a href="#sdfsdf">here</a>
+		</Accordion>
+
+		<hr />
+		<h2>Closed accordion</h2>
+
+		<Accordion open="false">
+			Some content of the accordion <a href="#sdfsdf">here</a>
+		</Accordion>
+	</div>,
 
 	document.getElementById('root'),
 );
