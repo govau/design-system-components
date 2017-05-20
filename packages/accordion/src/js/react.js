98a9c4527fe3a74a2f95a640d3553aacdeea2209
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-05-20 16:18:43 +1000

    added state example, missing pre and post functions
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- packages/accordion/src/js/react.js
+++ packages/accordion/src/js/react.js
@@ -10,123 +10,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// const Accordion = () => (
-// 	<div className="accordion">
-// 		<h1>I&rsquo;m an accordion</h1>
-// 	</div>
-// );
-
-
-
-
-// export default Accordion;
-
-
-
-
-
-
-// import "accodrdion.scss"
-
-// export const Accordion = () => (
-
-
-// 	<section class="uikit-accordion">
-// 		<a href="#accordion-default"
-// 			class="uikit-accordion__title js-uikit-accordion"
-// 			aria-controls="accordion-default"
-// 			aria-expanded="true"
-// 			aria-selected="true"
-// 			role="tab"
-// 			onclick={props.toggle}>
-// 				Accordion title
-// 		</a>
-
-// 		<div class="uikit-accordion__body" id="accordion-default" aria-hidden="false">
-// 			<div class="uikit-accordion__body-wrapper">
-
-// 				Here <a href="#url">is</a> some accordion content
-
-// 			</div>
-// 		</div>
-// 	</section>
-// )
-
-
-
-
-// import Accordion from '../';
-
-
-// const withOtherStuff = Component => {
-// 	return ({toggle, ...rest}) => {
-// 		const otherStuffToggle = () => {
-// 			console.log('dskjfjdshf');
-// 			toggle();
-// 		};
-//   	return <Component toggle={otherStuffToggle} {...rest} />
-// 	}
-// }
-
-
-// const withToggle = Component => {
-
-// 	let activePaneId = 1;
-
-
-// 	return ({toggle, ...rest}) => {
-// 		const otherStuffToggle = () => {
-// 			console.log('dskjfjdshf');
-// 			toggle();
-// 		};
-//   	return <Component toggle={otherStuffToggle} {...rest} />
-// 	}
-// }
-
-// const MyAccordion = withToggle(Accordion);
-
-// return <MyAccordion />
-
-
-
-
-
-// const HorizAccordion = ({onOpenPane, isOpen}) => {
-// 	return (
-// 		<div>
-// 			<div onClick={onOpenPane}>
-// 			</div>
-// 		</div>
-// 	)
-// }
-// export  withAccordion(HorizAccordion);
-
-// const VertAccordion = ({onOpenPane, isOpen}) => {
-// 	return (
-// 		<div>
-// 			<div onClick={onOpenPane}>
-// 			</div>
-// 		</div>
-// 	)
-// }
-
-
-// const withAccordion = Component => {
-// 	return class extends React.Component {
-// 		constructor(props) {
-// 			super(props)
-// 			this.state = {
-// 				open: false,
-// 			}
-// 		}
-// 		openPane() {
-// 			this.setState({open, !this.state.open});
-// 		}
-// 		render() {
-// 			return <Component onOpenPane={this.openPane.bind(this)} isOpen={this.state.open} />
-// 		}
-// 	}
-// }
 
 let IDstart = 0;
 
@@ -134,7 +17,7 @@ export class Accordion extends React.PureComponent {
 	componentWillReceiveProps( nextProps ) {
 		const open = nextProps.open;
 
-		if( open ) {
+		if( open === 'true' ) {
 			UIKIT.accordion.Open( this.accordionHeader );
 		}
 		else {
@@ -144,7 +27,8 @@ export class Accordion extends React.PureComponent {
 
 	componentWillMount() {
 		IDstart ++;
-		this.ID = IDstart;
+		this.ID = `accordion${ IDstart }`;
+		this.closeClass = this.props.open === 'false' ? 'uikit-accordion--closed' : '';
 	}
 
 	toggle( event ) {
@@ -158,14 +42,11 @@ export class Accordion extends React.PureComponent {
 	}
 
 	render() {
-		const ID = `accordion${ this.ID }`;
-		const closeClass = this.props.open === 'false' ? 'uikit-accordion--closed' : '';
-
 		return (
 			<section className="uikit-accordion">
-				<a href={`"#${ ID }"`}
-					className={`uikit-accordion__title js-uikit-accordion ${ closeClass }`}
-					aria-controls={ ID }
+				<a href={`"#${ this.ID }"`}
+					className={`uikit-accordion__title js-uikit-accordion ${ this.closeClass }`}
+					aria-controls={ this.ID }
 					aria-expanded={ this.props.open === 'false' ? 'false' : 'true' }
 					aria-selected={ this.props.open === 'false' ? 'false' : 'true' }
 					role="tab"
@@ -175,8 +56,8 @@ export class Accordion extends React.PureComponent {
 				</a>
 
 				<div
-					className={`uikit-accordion__body ${ closeClass }`}
-					id={ ID }
+					className={`uikit-accordion__body ${ this.closeClass }`}
+					id={ this.ID }
 					aria-hidden={ this.props.open === 'false' ? 'true' : 'false' }>
 					<div className="uikit-accordion__body-wrapper">
 
@@ -191,19 +72,9 @@ export class Accordion extends React.PureComponent {
 
 Accordion.propTypes = {
 	children: PropTypes.node.isRequired,
+	header: PropTypes.string.isRequired,
 };
 
 Accordion.defaultProps = {
 	open: 'true',
 };
-
-
-
-// <Accordian open={this.state.open} onClick={() => this.setState({open: !this.state.open})} header="sdhfgkjdshb">
-// 	sdf;dsjhfkjdsahbf lksdfjhg
-// </Accordian>
-
-
-// <Accordian header="sdhfgkjdshb">
-// 	sdf;dsjhfkjdsahbf lksdfjhg
-// </Accordian>
--- packages/accordion/tests/react/index.js
+++ packages/accordion/tests/react/index.js
@@ -3,32 +3,67 @@ import ReactDOM from 'react-dom';
 
 import { Accordion } from './react.js';
 
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			accordion1: 'true',
+			accordion2: 'false',
+		};
+	}
+
+	changeAccordion( item ) {
+		const accordionState = this.state[ item ] === 'false' ? 'true' : 'false';
+
+		this.setState({ [item]: accordionState });
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Default accordion</h2>
+
+				<Accordion header="Open and close me">
+					Some content of the accordion <a href="#sdfsdf">here</a> :)
+				</Accordion>
+
+				<hr />
+				<h2>Accordions with open/close props</h2>
+
+				<Accordion open="false" header="Closed">
+					Some content of the accordion <a href="#sdfsdf">here</a> :D
+				</Accordion>
+
+				<Accordion open="true" header="Open">
+					Some content of the accordion <a href="#sdfsdf">here</a> 8)
+				</Accordion>
+
+				<hr />
+				<h2>Accordions with custom functions</h2>
+
+				<Accordion open="false" header="With custom function" onOpen={ () => { console.log('yay!'); } }>
+					Some content of the accordion <a href="#sdfsdf">here</a>
+				</Accordion>
+
+				<hr />
+				<h2>Accordions with custom functions and state</h2>
+
+				<Accordion open={ this.state.accordion1 } header="State controlled accordion">
+					Some content of the accordion <a href="#sdfsdf">here</a>
+				</Accordion>
+
+				<button type="button" onClick={ () => { this.changeAccordion('accordion1') } }>Toggle accordion via state</button>
+			</div>
+		)
+	}
+};
+
+
+
 ReactDOM.render(
-	<div>
-		<h2>Default accordion</h2>
-
-		<Accordion header="Open and close me">
-			Some content of the accordion <a href="#sdfsdf">here</a> :)
-		</Accordion>
-
-		<hr />
-		<h2>Accordions with open/close props</h2>
-
-		<Accordion open="false" header="Closed">
-			Some content of the accordion <a href="#sdfsdf">here</a> :D
-		</Accordion>
-
-		<Accordion open="true" header="Open">
-			Some content of the accordion <a href="#sdfsdf">here</a> 8)
-		</Accordion>
-
-		<hr />
-		<h2>Accordions with custom open function</h2>
-
-		<Accordion open="false" header="With custom function" onOpen={ () => { console.log('yay!'); } }>
-			Some content of the accordion <a href="#sdfsdf">here</a>
-		</Accordion>
-	</div>,
+	<App />,
 
 	document.getElementById('root'),
 );
