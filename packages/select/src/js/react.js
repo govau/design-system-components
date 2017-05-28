a600b12f46eae25e6953fbdb3758c3e184733d24
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-05-28 15:32:29 +1000

    added select
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

---
+++ packages/select/src/js/react.js
@@ -0,0 +1,63 @@
+/*! [replace-name] v[replace-version] */
+/***************************************************************************************************************************************************************
+ *
+ * select function
+ *
+ * The form select element for multiple options
+ *
+ **************************************************************************************************************************************************************/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+/**
+ * DEFAULT
+ * The select component
+ *
+ * @param  {string}   options  - The options for the select, format: { value: '', text: '' }
+ * @param  {string}   id       - The ID for the select for the label
+ * @param  {string}   name     - The name attribute
+ * @param  {string}   value    - An optional pre-selected value, needs onChange
+ * @param  {function} onChange - A function to be called on change
+ * @param  {string}   block    - The block option
+ */
+const Select = ({ options, id, name, value, onChange, block }) => {
+
+	const attributeOptions = {};
+
+	if( typeof id !== 'undefined' ) {
+		attributeOptions.id = id;
+	}
+
+	if( typeof name !== 'undefined' ) {
+		attributeOptions.name = name;
+	}
+
+	if( typeof value !== 'undefined' ) {
+		attributeOptions.value = value;
+	}
+
+	if( typeof onChange === 'function' ) {
+		attributeOptions.onChange = onChange;
+	}
+
+	return (
+		<div className={`uikit-select${ block ? ` uikit-select-block` : `` }`}>
+			<select className="uikit-select__element" { ...attributeOptions }>
+				{ options.map( ( option, i ) => <option key={ i } value={ option.value }>{ option.text }</option> ) }
+			</select>
+		</div>
+	);
+};
+
+Select.propTypes = {
+	options: PropTypes.array.isRequired,
+	name: PropTypes.string,
+	value: PropTypes.string,
+	id: PropTypes.string,
+	onChange: PropTypes.func,
+	block: PropTypes.bool,
+};
+
+export default Select;
---
+++ packages/select/tests/react/index.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Select from './select.js';
+
+
+// to test a select with state
+class SelectWrapper extends React.Component {
+	// for an example on what a state change might look like we have to add a state
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			select: '2',
+		};
+	}
+
+	// let’s change the state in the absence of more complex application code
+	changeSelect( value ) {
+		this.setState({ select: value });
+	}
+
+	render() {
+		return (
+			<div>
+				<Select onChange={ event => this.changeSelect( event.target.value ) } value={ this.state.select } options={[
+					{
+						value: '',
+						text: 'Please select',
+					},
+					{
+						value: '1',
+						text: 'Option 1',
+					},
+					{
+						value: '2',
+						text: 'Option 2',
+					},
+					{
+						value: '3',
+						text: 'Option 3',
+					},
+					{
+						value: '4',
+						text: 'Option 4',
+					},
+				]} />
+
+				<br /><button type="button" onClick={ () => { this.changeSelect('3') } }>Change select to value 3</button>
+			</div>
+		);
+	};
+}
+
+
+ReactDOM.render(
+	<div>
+		<h2>select default</h2>
+
+		<Select options={[
+			{
+				value: '',
+				text: 'Please select',
+			},
+			{
+				value: '1',
+				text: 'Option 1',
+			},
+			{
+				value: '2',
+				text: 'Option 2',
+			},
+			{
+				value: '3',
+				text: 'Option 3',
+			},
+			{
+				value: '4',
+				text: 'Option 4',
+			},
+		]} />
+
+
+		<hr />
+		<h2>select block</h2>
+
+		<Select block options={[
+			{
+				value: '',
+				text: 'Please select',
+			},
+			{
+				value: '1',
+				text: 'Option 1',
+			},
+			{
+				value: '2',
+				text: 'Option 2',
+			},
+			{
+				value: '3',
+				text: 'Option 3',
+			},
+			{
+				value: '4',
+				text: 'Option 4',
+			},
+		]} />
+
+
+		<hr />
+		<h2>select with onchange</h2>
+
+		<Select onChange={ () => {
+			console.log('This function is called when the select input has changed');
+		} } options={[
+			{
+				value: '1',
+				text: 'Option 1',
+			},
+			{
+				value: '2',
+				text: 'Option 2',
+			},
+			{
+				value: '3',
+				text: 'Option 3',
+			},
+		]} />
+
+
+		<hr />
+		<h2>select with state</h2>
+
+		<SelectWrapper />
+	</div>,
+
+	document.getElementById('root'),
+);
