f41b52dbd19df7c48838f3175f3d488709dec429
Steven Worley <sj.worley88@gmail.com>
2017-06-20 15:41:19 +1000

    Horizontal List module
    - Adds horizontal list module as per UI Kit v1.0 outlines.
    
    @see http://guides.service.gov.au/design-guide/components/list-styles/index.html#horizontal-style

---
+++ packages/horizontal-list/tests/react/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HorizontalList from './horizontal-list.js';
+
+
+ReactDOM.render(
+	<div>
+		<Horizontal-list>
+		</Horizontal-list>
+	</div>,
+
+	document.getElementById('root'),
+);
