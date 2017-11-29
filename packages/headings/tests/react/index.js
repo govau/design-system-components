55fe23c9913f2e9883a94ceb111970f14dfeb244
Alex Page <alex@alexpage.com.au>
2017-11-29 15:58:01 +1100

    Adding dark test for heading react component

--- packages/headings/tests/react/index.js
+++ packages/headings/tests/react/index.js
@@ -5,16 +5,29 @@ import Heading from './headings.js';
 
 
 ReactDOM.render(
-	<div>
-		<h2>headings</h2>
+	<div className="split-wrapper">
+		<div className="split">
+			<h2>headings</h2>
 
-		<Heading size="xxxl" level="6" text="au-display-xl on h1" />
-		<Heading size="xxl"  level="5" text="au-display-xl on h1" />
-		<Heading size="xl"   level="1" text="au-display-xl on h1" />
-		<Heading size="lg"   level="2" text="au-display-lg on h2" />
-		<Heading size="md"   level="3" text="au-display-md on h3" />
-		<Heading size="sm"   level="6" text="au-display-sm on h6" />
-		<Heading size="xs"   level="4" text="au-display-xs on h4" />
+			<Heading size="xxxl" level="6" text="au-display-xl on h1" />
+			<Heading size="xxl"  level="5" text="au-display-xl on h1" />
+			<Heading size="xl"   level="1" text="au-display-xl on h1" />
+			<Heading size="lg"   level="2" text="au-display-lg on h2" />
+			<Heading size="md"   level="3" text="au-display-md on h3" />
+			<Heading size="sm"   level="6" text="au-display-sm on h6" />
+			<Heading size="xs"   level="4" text="au-display-xs on h4" />
+		</div>
+		<div className="split split--dark">
+			<h2>headings <code>--dark</code></h2>
+
+			<Heading size="xxxl" level="6" text="au-display-xl on h1" />
+			<Heading size="xxl"  level="5" text="au-display-xl on h1" />
+			<Heading size="xl"   level="1" text="au-display-xl on h1" />
+			<Heading size="lg"   level="2" text="au-display-lg on h2" />
+			<Heading size="md"   level="3" text="au-display-md on h3" />
+			<Heading size="sm"   level="6" text="au-display-sm on h6" />
+			<Heading size="xs"   level="4" text="au-display-xs on h4" />
+		</div>
 	</div>,
 
 	document.getElementById('root'),
