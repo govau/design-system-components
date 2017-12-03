cdc186499092c65e33ecd7a7fa9d08ed1498e4da
Alex Page <alex@alexpage.com.au>
2017-12-04 08:06:36 +1100

    Fixing title text

--- packages/headings/tests/react/index.js
+++ packages/headings/tests/react/index.js
@@ -9,8 +9,8 @@ ReactDOM.render(
 		<div className="split">
 			<h2>headings</h2>
 
-			<Heading size="xxxl" level="6" text="au-display-xl on h1" />
-			<Heading size="xxl"  level="5" text="au-display-xl on h1" />
+			<Heading size="xxxl" level="6" text="au-display-xxxl on h1" />
+			<Heading size="xxl"  level="5" text="au-display-xxl on h1" />
 			<Heading size="xl"   level="1" text="au-display-xl on h1" />
 			<Heading size="lg"   level="2" text="au-display-lg on h2" />
 			<Heading size="md"   level="3" text="au-display-md on h3" />
@@ -20,8 +20,8 @@ ReactDOM.render(
 		<div className="split split--dark">
 			<h2>headings <code>--dark</code></h2>
 
-			<Heading size="xxxl" level="6" text="au-display-xl on h1" />
-			<Heading size="xxl"  level="5" text="au-display-xl on h1" />
+			<Heading size="xxxl" level="6" text="au-display-xxxl on h1" />
+			<Heading size="xxl"  level="5" text="au-display-xxl on h1" />
 			<Heading size="xl"   level="1" text="au-display-xl on h1" />
 			<Heading size="lg"   level="2" text="au-display-lg on h2" />
 			<Heading size="md"   level="3" text="au-display-md on h3" />
