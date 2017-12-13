20907436c6fb215cb5adc60147bf59a877075169
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2017-12-13 13:27:48 +1100

    added pancake config test
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- scripts/helper.js
+++ scripts/helper.js
@@ -1043,6 +1043,11 @@ HELPER.test = (() => {
 						error += `The module ${ module } doesn’t have the right amount of devDependencies.\n`;
 					}
 
+					// testing for pancake config
+					if( packagesPKG.pancake['auto-save'] !== undefined ) {
+						error += `The module ${ module } has the pancake config saved though we don’t want that…\n`;
+					}
+
 				}
 			}
 
