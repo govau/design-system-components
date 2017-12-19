4f5bc7feb52099e83a491bb90b74a07a38ef7dec
Alex Page <alex@alexpage.com.au>
2017-12-19 14:27:06 +1100

    Page alerts adding alt and dark

--- packages/page-alerts/src/js/react.js
+++ packages/page-alerts/src/js/react.js
@@ -32,17 +32,24 @@ const options = {
  * DEFAULT
  * Page alert
  *
+ * @param  {string} dark             - Add the dark variation class
+ * @param  {string} alt              - Add the alt variation class
  * @param  {string} as               - What kind of alert this is, takes: 'info', 'warning', 'error', 'success'
  * @param  {node}   children         - Anything inside the component
  * @param  {object} attributeOptions - Any other attribute options
  */
-const AUpageAlert = ({ as, children, ...attributeOptions }) => (
-	<div className={`au-body au-page-alerts ${ options[ as ] }`} role='alert' { ...attributeOptions }>
+const AUpageAlert = ({ as, alt, dark, children, ...attributeOptions }) => (
+	<div
+		className={`au-body au-page-alerts ${ dark ? ' au-page-alerts--dark au-body--dark' : '' }${ alt ? ' au-page-alerts--alt au-body--alt' : '' } ${ options[ as ] }`}
+		role='alert' { ...attributeOptions }
+	>
 		{ children }
 	</div>
 );
 
 AUpageAlert.propTypes = {
+	dark: PropTypes.bool,
+	alt: PropTypes.bool,
 	as: PropTypes.oneOf([ 'info', 'warning', 'error', 'success' ]).isRequired,
 	children: PropTypes.node.isRequired,
 };
