435ecd00a8b3cda4e7c6a902b73ca73e599e8d8b
Shakil Siraj <shakil.siraj@gmail.com>
2018-06-20 11:58:40 +1000

    More work ..

---
+++ packages/ngx/src/app/demo/accordion/accordion-demo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "app-accordion",
+  templateUrl: "./accordion-demo.component.html",
+  styleUrls: ["./accordion-demo.component.scss"]
+})
+export class AccordionDemoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
