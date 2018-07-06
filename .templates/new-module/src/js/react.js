8033cec794185cf11679449cf67b2ff1d2653d4b
Alex Page <alex@alexpage.com.au>
2018-07-06 12:05:02 +1000

    Update react template

--- .templates/new-module/src/js/react.js
+++ .templates/new-module/src/js/react.js
@@ -8,11 +8,19 @@
  **************************************************************************************************************************************************************/
 
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const AU[-replace-name-capital-] = () => (
-	<div className="uikit-[-replace-name-]">
+	<div className="au-[-replace-name-]">
 		<strong>Yay!</strong>
 	</div>
 );
+
+AU[-replace-name-capital-].propTypes = {
+	dark: PropTypes.bool
+};
+
+AU[-replace-name-capital-].defaultProps = {};
 
 export default AU[-replace-name-capital-];
