a2caf7d88c36a13887bd484e74aab8d9ec26c446
Alex Page <alex@alexpage.com.au>
2018-07-13 16:42:51 +1000

    JS code for resetting aria-hidden

--- packages/side-nav/src/js/module.js
+++ packages/side-nav/src/js/module.js
@@ -34,30 +34,62 @@ var AU = AU || {};
 		)[ property ]; // avoid getPropertyValue altogether
 	}
 
+	/**
+	 * PRIVATE
+	 * Debounce function to run a function after it has stopped being called for a certain amount of time
+	 */
+	function Debounce( runThisFunction, wait, immediate ) {
+		var timeout;
+
+		return function() {
+			var context = this;
+			var args    = arguments;
+
+			var later = function() {
+				timeout = null;
+				if (!immediate) runThisFunction.apply( context, args );
+			};
+
+			var callNow = immediate && !timeout;
+
+			clearTimeout( timeout );
+			timeout = setTimeout( later, wait );
+
+			if ( callNow ) {
+				runThisFunction.apply( context, args );
+			};
+		};
+	};
+
 
 	/**
 	 * PRIVATE
 	 * Toggle the aria-hidden based on if the menu is visible
 	 */
-	function ToggleAriaRoles( element ) {
-		var elementDisplay = GetStyle( element, 'display' );
+	function ToggleAriaRoles( elements ) {
+		for( var i = 0; i < elements.length; i++ ){
+			var elementDisplay = GetStyle( elements[ i ], 'display' );
 
-		if( elementDisplay === 'none' ) {
-			element.setAttribute( "aria-hidden", "true" );
-		}
-		else {
-			element.setAttribute( "aria-hidden", "false" );
+			if( elementDisplay === 'none' ) {
+				elements[ i ].setAttribute( "aria-hidden", "true" );
+			}
+			else {
+				elements[ i ].setAttribute( "aria-hidden", "false" );
+			}
 		}
 	};
 
 
+	var sideNavContent = document.querySelectorAll( '.au-side-nav__content' );
+
+
 	// Run on page load
-	ToggleAriaRoles();
+	ToggleAriaRoles( sideNavContent );
 
 
 	// Run PageResize function on resize
 	window.onresize = function() {
-		Debounce( ToggleAriaRoles(), 250 );
+		Debounce( ToggleAriaRoles( sideNavContent ), 250 );
 	}
 
 	AU.sideNav = sideNav;
