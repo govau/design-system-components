5516d82cf8df5b5c146b6a63e27132444312cdb9
Alex Page <alex@alexpage.com.au>
2018-07-13 15:58:18 +1000

    Some work towards aria-hidden when shown

--- packages/side-nav/src/js/module.js
+++ packages/side-nav/src/js/module.js
@@ -11,7 +11,58 @@ var AU = AU || {};
 
 
 // This is necessary to use accordion javascript
-( function( AU ) {}( AU ));
+( function( AU ) {
+
+	//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// NAMESPACE MODULE
+	//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+	var sideNav = {}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+// PRIVATE FUNCTIONS
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+	/**
+	 * PRIVATE
+	 * Get the style of the element
+	 */
+	function GetStyle( element, property ) {
+		return (
+			typeof getComputedStyle !== 'undefined'
+				? getComputedStyle( element, null )
+				: element.currentStyle
+		)[ property ]; // avoid getPropertyValue altogether
+	}
+
+
+	/**
+	 * PRIVATE
+	 * Toggle the aria-hidden based on if the menu is visible
+	 */
+	function ToggleAriaRoles( element ) {
+		var elementDisplay = GetStyle( element, 'display' );
+
+		if( elementDisplay === 'none' ) {
+			element.setAttribute( "aria-hidden", "true" );
+		}
+		else {
+			element.setAttribute( "aria-hidden", "false" );
+		}
+	};
+
+
+	// Run on page load
+	ToggleAriaRoles();
+
+
+	// Run PageResize function on resize
+	window.onresize = function() {
+		Debounce( ToggleAriaRoles(), 250 );
+	}
+
+	AU.sideNav = sideNav;
+
+}( AU ));
 
 
 if( typeof module !== 'undefined' ) {
