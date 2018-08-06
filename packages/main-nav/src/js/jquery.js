3df7d0f63e2c8a82587a683d9e5f2e69515039a1
Alex Page <alex@alexpage.com.au>
2018-08-06 13:08:08 +1000

    jquery round 1

--- packages/main-nav/src/js/jquery.js
+++ packages/main-nav/src/js/jquery.js
@@ -8,9 +8,42 @@
  **************************************************************************************************************************************************************/
 
 
+
+/**
+ * The main nav jquery function to add attributes and event listeners
+ *
+ * Public functions are:
+ */
 $.fn.AUmainNav = function() {
 	var $elements = this;
 
-	return {
+	var mainNavObject = {
+		addListeners: function() {
+			console.log( 'hello' );
+
+			$('.au-main-nav__toggle--open').on('click', function() {
+				var selector = $( this ).closest('.au-main-nav');
+				console.log( selector );
+				var speed = $( this ).attr('data-speed');
+
+				AU.mainNav.Open( $( selector ), speed );
+			});
+
+			// close all
+			$('.au-main-nav__toggle--close').not('.js-au-accordion-rendered').on('click', function() {
+				var selector = $( this ).closest('.au-main-nav');
+				console.log( selector );
+				var speed = $( this ).attr('data-speed');
+
+				AU.mainNav.Close( $( selector ), speed );
+			}).addClass('js-au-accordion-rendered');
+
+			return this;
+		},
 	};
+
+	mainNavObject
+		.addListeners();
+
+	return mainNavObject;
 };
