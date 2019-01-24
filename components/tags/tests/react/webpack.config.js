cd777a2485d6976f8a70744ca519ad88b2b39601
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-01-24 14:06:50 +1100

    Installed babel preset minify

--- components/tags/tests/react/webpack.config.js
+++ components/tags/tests/react/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 				      use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env','@babel/preset-react']
+          presets: ['@babel/preset-env','@babel/preset-react','minify']
         }
       }
 			},
