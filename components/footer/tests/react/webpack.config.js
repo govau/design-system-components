c24094b2459f386369c6fbe9994726e9b8412653
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-01-29 09:54:09 +1100

    Correct webpack config to reference babel-preset-minify as a plugin not preset

--- components/footer/tests/react/webpack.config.js
+++ components/footer/tests/react/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
 				      use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env','@babel/preset-react','minify']
+          presets: ['@babel/preset-env','@babel/preset-react'],
+          plugins: ['minify'],
         }
       }
 			},
