007aaf090f6cd8a9c553c2294b95baa32317927f
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-02-28 10:31:35 +1100

    Removed unused fragment import

--- packages/direction-links/tests/react/index.js
+++ packages/direction-links/tests/react/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
--- packages/side-nav/tests/react/index.js
+++ packages/side-nav/tests/react/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
