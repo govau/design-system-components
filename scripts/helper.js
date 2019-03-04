938060459ea88152442a10cf626024b17d1bc4c8
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-03-04 12:33:52 +1100

    Modified devDep tests

--- scripts/helper.js
+++ scripts/helper.js
@@ -1059,7 +1059,7 @@ HELPER.test = (() => {
 						error += `The module ${ module } is missing "react" as devDependency.\n`;
 					}
 
-					if( hasReact && Object.keys( packagesPKG.devDependencies ).length < 12 ) {
+					if( hasReact && Object.keys( packagesPKG.devDependencies ).length < 10 ) {
 						error += `The module ${ module } doesn’t have the right amount of devDependencies.\n`;
 					}
 
