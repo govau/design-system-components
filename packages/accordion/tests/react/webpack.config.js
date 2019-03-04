16a4160d6f40ec39964c4670fe3ce8c4a4a2bf57
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-03-04 12:17:52 +1100

    Upgraded webpack.config.js to support v 4

--- packages/accordion/tests/react/webpack.config.js
+++ packages/accordion/tests/react/webpack.config.js
@@ -1,7 +1,11 @@
+const path = require( 'path' );
+
 module.exports = {
 	entry: './index.js',
+	mode: 'development',
 	output: {
 		filename: './bundle.js',
+		path: path.resolve( __dirname )
 	},
 	module: {
 		rules: [
