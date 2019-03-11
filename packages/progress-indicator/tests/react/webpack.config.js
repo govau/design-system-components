4a586d31a681d42f08b1a8052202058a406f2227
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-03-12 10:37:36 +1100

    Upgraded webpack configs from 3 -> 4

--- packages/progress-indicator/tests/react/webpack.config.js
+++ packages/progress-indicator/tests/react/webpack.config.js
@@ -1,8 +1,12 @@
+const path = require( 'path' );
+
 module.exports = {
 	entry: './index.js',
+	mode: 'development',
 	output: {
 		filename: './bundle.js',
 	},
+	path: path.resolve( __dirname ),
 	module: {
 		rules: [
 			{
