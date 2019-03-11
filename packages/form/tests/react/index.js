f6fb967fb4ae594f219bfb44ef704788edb10d9f
sukhrajghuman <sukhrajghuman@live.com>
2019-03-12 08:43:37 +1100

    add text input,  css loader dev deps, and bootstrap

--- packages/form/tests/react/index.js
+++ packages/form/tests/react/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import AUlabel from './form.js';
+import AUtextInput from '@gov.au/text-inputs';
 
-
-ReactDOM.render(<AUlabel title="Password" htmlFor="something" />,
+ReactDOM.render(<div><AUlabel title="Password" htmlFor="something" />
+<AUtextInput id="text-input" /></div>,
 
 	document.getElementById('root'),
 );
--- packages/form/tests/react/webpack.config.js
+++ packages/form/tests/react/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
 				exclude: /node_modules/,
 				use: 'babel-loader',
 			},
+			{
+				test: /\.(css|scss)$/,
+				use: ["css-loader"]
+			}
 		],
 	},
 };
