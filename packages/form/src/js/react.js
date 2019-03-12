2cba21757ed301d7a875ad2d78c4afe3390dd654
sukhrajghuman <sukhrajghuman@live.com>
2019-03-12 11:44:22 +1100

    Add react implentation
    Add react implementation for
    - labels
    - errors
    -hint text
    -fieldsets
    - form groups
    
    also added tests

--- packages/form/src/js/react.js
+++ packages/form/src/js/react.js
@@ -19,15 +19,15 @@ import PropTypes from 'prop-types';
 
 /**
  * DEFAULT
- * The footer component
+ * The label component
  *
+ * @param  {string}  title            - Title of the label, required
  * @param  {boolean} dark             - Add the dark variation class, optional
- * @param  {boolean} alt              - Add the alt variation class, optional
- * @param  {node}    children         - The inside of this section
+ * @param  {boolean} inline           - Display the label inline, optional
  * @param  {string}  className        - An additional class, optional
  * @param  {object}  attributeOptions - Any other attribute options
  */
-const AUlabel = ({ dark, inline, title,  className = '', ...attributeOptions }) => (
+export const AUlabel = ({ title, dark, inline, className = '', ...attributeOptions }) => (
 	<label
 		className={ `au-label ${ className }` +
 		`${ dark ? ' au-label--dark' : '' }` + 
@@ -40,9 +40,111 @@ const AUlabel = ({ dark, inline, title,  className = '', ...attributeOptions }) 
 );
 
 AUlabel.propTypes = {
+	title: PropTypes.string.isRequired,
+	dark: PropTypes.bool,
+	inline: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+/**
+ * DEFAULT
+ * The hint text component
+ *
+ * @param  {string}  title            - Title of the label, required
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUhintText = ({ title, dark,  className = '', ...attributeOptions }) => (
+	<span
+		className={ `au-hint-text ${ className }` +
+		`${ dark ? ' au-hint-text--dark' : '' }`
+		}
+		{ ...attributeOptions }
+	>
+    {title}
+	</span>
+);
+
+AUhintText.propTypes = {
+	title: PropTypes.string.isRequired,
+	dark: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+
+
+/**
+ * The error component
+ *
+ * @param  {string}  title            - Title of the label, required
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUerrorText = ({ title, dark, className = '', ...attributeOptions }) => (
+	<span
+		className={ `au-error-text ${ className }` +
+		`${ dark ? ' au-error-text--dark' : '' }`
+		}
+		{ ...attributeOptions }
+	>
+    {title}
+	</span>
+);
+
+AUerrorText.propTypes = {
 	dark: PropTypes.bool,
 	title: PropTypes.string.isRequired,
 	className: PropTypes.string,
 };
 
-export default AUlabel;
+
+/**
+ * The form group component
+ *
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUformGroup = ({ status, dark, className = '', ...attributeOptions }) => (
+	<div
+		className={ `au-form-group ${ className }` +
+		`${ dark ? ' au-form-group--dark' : '' }` + 
+		`${ status === 'invalid' ? 'au-form-group--invalid ' : ' '}`
+		}
+		{ ...attributeOptions }
+	>
+
+	</div>
+);
+
+AUformGroup.propTypes = {
+	status: PropTypes.oneOf(['valid', 'invalid']),
+	dark: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+
+/**
+ * The fieldset component
+ *
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUfieldset = ({ dark, className = '', ...attributeOptions }) => (
+	<div
+		className={ `au-fieldset ${ className }` +
+		`${ dark ? ' au-fieldset--dark' : '' }`
+		}
+		{ ...attributeOptions }
+	>
+
+	</div>
+);
+
+AUfieldset.propTypes = {
+	dark: PropTypes.bool,
+	className: PropTypes.string,
+};
