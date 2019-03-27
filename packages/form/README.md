bec42b53bed7969b34f9d86a5079bc5a264a4c68
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-03-27 14:13:00 +1100

    Added form component

---
+++ packages/form/README.md
@@ -0,0 +1,134 @@
+@gov.au/form
+============
+
+> 
+
+
+## Contents
+
+* [Install](#install)
+* [Dependency graph](#dependency-graph)
+* [Tests](#tests)
+* [Release History](#release-history)
+* [License](#license)
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## Install
+
+
+```shell
+yarn add @gov.au/form
+```
+
+```shell
+npm install @gov.au/form --save-dev
+```
+
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## Dependency graph
+
+```shell
+form
+└─ core
+```
+
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## Tests
+
+The visual test: https://auds.service.gov.au/packages/form/tests/site/
+
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## Release History
+
+* v0.1.0 - 💥 Initial version
+
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## License
+
+Copyright (c) Commonwealth of Australia.
+Licensed under [MIT](https://raw.githubusercontent.com/govau/design-system-components/packages/core/master/LICENSE).
+
+
+**[⬆ back to top](#contents)**
+
+# };
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## Usage
+
+
+* [React](#react)
+
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+### React
+
+Usage:
+
+```jsx
+import { AUhintText } from '@gov.au/form';
+<AUlabel title="Email" />
+<AUhintText title="We will only use this email to respond to your query." />
+<AUerrorText title="Enter an email address in the correct format, like name@example.com" />
+
+<AUformGroup>
+    // labels, hint text, and form controls here
+</AUformGroup>
+
+<AUfieldset>
+    // legend, form controls in here.
+</AUfieldset>
+
+
+```
+
+All props:
+
+```jsx
+<AUlabel 
+title="Email" {/* The text of the label */}
+dark = { true }              {/* The dark variation of the component */}
+inline={ true }              {/* Label inline*/}
+/>
+
+<AUhintText
+title="We will only use this email to respond to your query."
+/>
+
+<AUerrorText  />
+
+```
---
+++ packages/form/src/js/react.js
@@ -0,0 +1,166 @@
+/*! [replace-name] v[replace-version] */
+/***************************************************************************************************************************************************************
+ *
+ * footer function
+ *
+ * Footers help users who reach the bottom of a page without finding what they want.
+ *
+ **************************************************************************************************************************************************************/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+// The following line will be replaced automatically with generic imports for the ES5 pipeline.
+// You can safely ignore this bit if you use this module with pancake
+//
+// [replace-imports]
+
+
+/**
+ * DEFAULT
+ * The label component
+ *
+ * @param  {string}  text            - Text of the label, required
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {boolean} inline           - Display the label inline, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUlabel = ({ text, dark, inline, className = '', ...attributeOptions }) => (
+	<label
+		className={ `au-label ${ className }` +
+		`${ dark ? ' au-label--dark' : '' }` + 
+		`${ inline ? ' au-label--inline' : ''}`
+		}
+		{ ...attributeOptions }
+	>
+    { text }
+	</label>
+);
+
+AUlabel.propTypes = {
+	title: PropTypes.string.isRequired,
+	dark: PropTypes.bool,
+	inline: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+
+/**
+ * DEFAULT
+ * The hint text component
+ *
+ * @param  {string}  text            - Text of the label, required
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUhintText = ({ text, dark,  className = '', ...attributeOptions }) => (
+	<span className={ `au-hint-text ${ className }` + `${ dark ? ' au-hint-text--dark' : '' }`
+		}
+		{ ...attributeOptions }
+	>
+    { text }
+	</span>
+);
+
+AUhintText.propTypes = {
+	title: PropTypes.string.isRequired,
+	dark: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+
+
+/**
+ * The error text component
+ *
+ * @param  {string}  title            - Title of the label, required
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUerrorText = ({ text, dark, inline, className = '', ...attributeOptions }) => (
+	<span
+		className={ `au-error-text ${ className }` +
+		`${ dark ? ' au-error-text--dark' : '' }` +
+		`${ inline ? ' au-error-text--inline' : '' }`		
+		}
+		{ ...attributeOptions }
+	>
+	{ text }
+	</span>
+);
+
+AUerrorText.propTypes = {
+	dark: PropTypes.bool,
+	title: PropTypes.string.isRequired,
+	className: PropTypes.string,
+};
+
+
+/**
+ * The form group component
+ *
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUformGroup = ({ status, dark, className = '', ...attributeOptions }) => (
+	<div
+		className={ `au-form-group ${ className }` +
+		`${ dark ? ' au-form-group--dark' : '' }` + 
+		`${ status === 'invalid' ? 'au-form-group--invalid ' : ' '}`
+		}
+		{ ...attributeOptions }
+	>
+
+	</div>
+);
+
+AUformGroup.propTypes = {
+	status: PropTypes.oneOf(['valid', 'invalid']),
+	dark: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+AUformGroup.defaultProps = {
+	status: "valid"
+}
+
+
+/**
+ * The fieldset component
+ *
+ * @param  {boolean} dark             - Add the dark variation class, optional
+ * @param  {string}  className        - An additional class, optional
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUfieldset = ({ dark, className = '', ...attributeOptions }) => (
+	<fieldset
+		className={ `au-fieldset ${ className }` +
+		`${ dark ? ' au-fieldset--dark' : '' }`
+		}
+		{ ...attributeOptions }
+	>
+
+	</fieldset>
+);
+
+AUfieldset.propTypes = {
+	dark: PropTypes.bool,
+	className: PropTypes.string,
+};
+
+
+
+/**
+ * The form component wrapper
+ * 
+ * @param  {object}  attributeOptions - Any other attribute options
+ */
+export const AUForm = ({...attributeOptions }) => (
+	<form { ...attributeOptions } ></form>
+);
