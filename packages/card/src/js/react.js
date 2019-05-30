70e11c3bae292cbfa2958b767bc9ae56cf50b777
sukhrajghuman <sukhrajghuman@live.com>
2019-05-30 11:36:27 +1000

    Add react examples, remove jquery, js

--- packages/card/src/js/react.js
+++ packages/card/src/js/react.js
@@ -11,16 +11,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const AUCard = () => (
-	<div className="au-card">
-		<strong>Yay!</strong>
-	</div>
-);
+const AUCard = ({children, link, ...attributesOptions}) => {
+	let CardContainer = 'div';
+
+	if( link !== undefined ) {
+		CardContainer = 'a';
+		attributesOptions.href = link;
+	}
+
+	return (
+		<CardContainer className="au-card">
+			{children}
+		</CardContainer>
+	)
+};
 
 AUCard.propTypes = {
 	dark: PropTypes.bool
 };
 
 AUCard.defaultProps = {};
+
+export const AUcardInner = ({ children, ...attributesOptions }) => (
+	<div className="au-card__inner" {...attributesOptions}>
+		{children}
+	</div>
+);
+
+export const AUcardImage = ({ src, ...attributesOptions }) => (
+	<img className="au-card__image" src={ src } {...attributesOptions} />
+)
+
+export const AUcardDivider = ({ className, ...attributesOptions }) => (
+	<hr className="au-card__divider" {...attributesOptions} />
+)
 
 export default AUCard;
--- packages/card/tests/react/index.js
+++ packages/card/tests/react/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AUCard from './card.js';
-
+import AUCard, { AUcardInner, AUcardImage, AUcardDivider } from './card.js';
 
 ReactDOM.render(
-	<div>
-		<AUCard>
-		</AUCard>
+	<div className="split-wrapper">
+		<div className="split">
+			<AUCard>
+				hello
+			</AUCard>
+			<br/><br/>
+
+			<AUCard>
+				<h1>Some Heading</h1>
+				<AUcardDivider />
+				<p>Additional content</p>
+			</AUCard>
+			<br/><br/>
+
+			<AUCard className="bla" link="hello">
+				<AUcardInner>
+					Bla
+				</AUcardInner>
+			</AUCard>
+
+			<br/><br/>
+
+			<AUCard className="bla">
+				<AUcardInner>
+					<AUcardImage src="http://placehold.it/1200x500" />
+					Bla
+				</AUcardInner>
+			</AUCard>
+
+			<br/><br/>
+
+			<AUCard className="bla">
+				<AUcardImage src="http://placehold.it/1200x500" />
+					<AUcardInner>
+						Bla
+					</AUcardInner>
+			</AUCard>
+
+		</div>
 	</div>,
 
 	document.getElementById('root'),
