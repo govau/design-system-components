a81820f42d9d68ad6cb20af7067c24bed7749a9b
sukhrajghuman <sukhrajghuman@live.com>
2019-06-11 16:32:27 +1000

    Add react doc, update examples

--- packages/card/src/js/react.js
+++ packages/card/src/js/react.js
@@ -10,46 +10,168 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
-const AUcard = ({children, link, ...attributesOptions}) => {
+/**
+ * The card  component
+ *
+ * @param {string}   link               - The link that clicking the card goes to
+ * @param {string}   shadow             - Adds a box shadow to card if true
+ * @param {string}   centered           - Aligns all text to center if true
+ * @param {string}   className          - An additional class, optional
+ * @param {object}   attributeOptions   - Default HTML attributes
+ */
+const AUcard = ({ link, shadow, centered, className, children, ...attributesOptions }) => {
 	let CardContainer = 'div';
 
 	if( link !== undefined ) {
 		CardContainer = 'a';
 		attributesOptions.href = link;
+		console.log(CardContainer);
 	}
 
 	return (
-		<CardContainer className="au-card">
+		<CardContainer className={`au-card ${className} ` +
+								`${shadow ? 'au-card--shadow' : ''} ` +
+								`${centered ? 'au-card--centered' : ''}`}
+								{...attributesOptions}
+								>
 			{children}
 		</CardContainer>
 	)
 };
 
 AUcard.propTypes = {
-	dark: PropTypes.bool
+	link: PropTypes.string,
+	shadow: PropTypes.bool,
+	centered: PropTypes.bool,
+	className: PropTypes.string,
 };
 
-AUcard.defaultProps = {};
+AUcard.defaultProps = {
+	shadow: false,
+	centered: false,
+	className: ''
+};
 
-export const AUcardInner = ({ children, ...attributesOptions }) => (
-	<div className="au-card__inner" {...attributesOptions}>
+
+/**
+ * @param {string}   className          - An additional class, optional
+ * @param {object}   attributeOptions   - Default HTML attributes
+ */
+export const AUcardInner = ({ className, children, ...attributesOptions }) => (
+	<div className={`au-card__inner ${className}`} {...attributesOptions}>
 		{children}
 	</div>
 );
 
-export const AUcardImage = ({ src, ...attributesOptions }) => (
-	<img className="au-card__image" src={ src } {...attributesOptions} />
-)
+AUcardInner.propTypes = {
+	className: PropTypes.string
+}
 
+AUcardInner.defaultProps = {
+	className: ''
+}
+
+
+/**
+ * @param {string}   src                - Image source
+ * @param {string}   className          - An additional class, optional
+ * @param {object}   attributeOptions   - Default HTML attributes
+ */
+export const AUcardImage = ({ src,className, ...attributesOptions }) => (
+	<img className={`au-card__image ${className}`} src={ src } {...attributesOptions} />
+);
+
+AUcardImage.propTypes = {
+	src: PropTypes.string.isRequired,
+	className: PropTypes.string
+};
+
+AUcardImage.defaultProps = {
+	className: ''
+};
+
+
+/**
+ * @param {string}   className          - An additional class, optional
+ * @param {object}   attributeOptions   - Default HTML attributes
+ */
 export const AUcardDivider = ({ className, ...attributesOptions }) => (
-	<hr className="au-card__divider" {...attributesOptions} />
-)
+	<hr className={`au-card__divider ${className}`} {...attributesOptions} />
+);
 
-export const AUcardLink = ({ link, text,  className, ...attributesOptions }) => (
-	<a href={link} className="au-card__link" {...attributesOptions}>
+AUcardDivider.propTypes = {
+	className: PropTypes.string
+};
+
+AUcardDivider.defaultProps = {
+	className: ''
+};
+
+
+
+/**
+ * @param {string}   link               - The location of the link
+ * @param {string}   className          - An additional class, optional
+ * @param {object}   attributeOptions   - Default HTML attributes
+ */
+export const AUcardLink = ({ link, text, className, ...attributesOptions }) => (
+	<a href={link} className={`au-card__link ${className}`} {...attributesOptions}>
 		{text}
 	</a>
-)
+);
+
+AUcardLink.propTypes = {
+	link: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
+	className: PropTypes.string
+};
+
+AUcardLink.defaultProps = {
+	className: ''
+};
+
+
+
+
+
+/**
+ * @param {string}   className          - An additional class, optional
+ * @param {object}   attributeOptions   - Default HTML attributes
+ */
+export const AUcardFooter = ({ children, className, ...attributesOptions }) => (
+	<div className={`au-card__footer ${className}`} {...attributesOptions}>
+		{children}
+	</div>
+);
+
+AUcardFooter.propTypes = {
+	className: PropTypes.string
+};
+
+AUcardFooter.defaultProps = {
+	className: ''
+};
+
+
+
+/**
+ * @param {string}   className          - An additional class, optional
+ * @param {object}   attributeOptions   - Default HTML attributes
+ */
+export const AUcardBody = ({ children, className, ...attributesOptions }) => (
+	<div className={`au-card__body ${className}`} {...attributesOptions}>
+		{children}
+	</div>
+);
+
+AUcardBody.propTypes = {
+	className: PropTypes.string
+};
+
+AUcardBody.defaultProps = {
+	className: ''
+};
+
+
 
 export default AUcard;
--- packages/card/tests/react/index.js
+++ packages/card/tests/react/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AUcard, { AUcardInner, AUcardImage, AUcardDivider, AUcardLink } from './card.js';
+import AUcard, { AUcardInner, AUcardImage, AUcardDivider, AUcardLink, AUcardFooter } from './card.js';
 
 ReactDOM.render(
+	<div>
 	<div className="row">
 		<div className="col-md-4">
 
 			<h2>Basic card</h2><br/>
 			<AUcard>
 				A basic card
+			</AUcard>
+			<br/><br/>
+
+			<h2>Basic card with shadow</h2><br/>
+			<AUcard shadow={true}>
+				A basic card
+			</AUcard>
+			<br/><br/>
+
+			<h2>Basic card with centered content</h2><br/>
+			<AUcard centered={true}>
+			<AUcardImage src="http://placehold.it/1200x500" />
+				A basic card
+
 			</AUcard>
 			<br/><br/>
 
@@ -27,7 +42,7 @@ ReactDOM.render(
 			<AUcard>
 				<AUcardInner>
 					<AUcardImage src="http://placehold.it/1200x500" />
-					Bla
+					Image caption
 				</AUcardInner>
 			</AUcard>
 
@@ -42,16 +57,28 @@ ReactDOM.render(
 			</AUcard>
 
 			<br/><br/>
+
+			<h2>Card with full width image and footer</h2>
+			<AUcard className="au-body">
+				<AUcardImage src="http://placehold.it/1200x500" />
+					<AUcardInner>
+						<span>Image caption or description</span>
+						<AUcardFooter>
+							<a href="#">Call to action</a>
+						</AUcardFooter>
+					</AUcardInner>
+			</AUcard>
+
 		</div>
 
 
 		<div className="col-md-4 col-md-offset-2">
 
 		<h2>Card with image and wrappingLink</h2>
-		<AUcard className="bla">
+		<AUcard>
 			<AUcardImage src="http://placehold.it/1200x500" />
 				<AUcardInner>
-						<h3 className="au-card__title"><AUcardLink link="#" text="Some link" /></h3>
+						<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
 				</AUcardInner>
 		</AUcard>
 		<br/><br/>
@@ -59,10 +86,81 @@ ReactDOM.render(
 		<AUcard link="#">
 		<AUcardImage src="http://placehold.it/1200x500" />
 			<AUcardInner>
-					<h3 className="au-card__title"><AUcardLink link="#" text="Some link" /></h3>
+					<h3 className="au-card__title">Title of article</h3>
 			</AUcardInner>
 		</AUcard>
 		</div>
+	</div>
+
+	<div className="row">
+		<h2>Card List example</h2>
+			<ul className="au-card-list au-card-list--matchheight">
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+				<li className="col-sm-3 col-xs-6">
+					<AUcard>
+						<AUcardImage src="http://placehold.it/1200x500" />
+							<AUcardInner>
+									<h3 className="au-card__title au-body"><AUcardLink link="#" text="Some link" /></h3>
+							</AUcardInner>
+					</AUcard>
+				</li>
+			</ul>
+	</div>
 	</div>,
 
 	document.getElementById('root'),
