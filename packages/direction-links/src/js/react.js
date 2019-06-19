6552d0acc070c9e847da41f50bc3dd4312590fcf
Raj <sukhrajghuman@live.com>
2019-06-20 09:16:12 +1000

    Update packages/direction-links/src/js/react.js

--- packages/direction-links/src/js/react.js
+++ packages/direction-links/src/js/react.js
@@ -90,7 +90,6 @@ AUdirectionLink.defaultProps = {
 
 /**
  * The inner contents of the direction link. Includes the text and and the arrow.
- *
  * @param  {string}  text             - The text of the direction link
  * @param  {string}  direction        - The direction for the arrow; can be either: up right down left, default: 'right'
  */
