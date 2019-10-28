cb546312bee1ed2be3d51a77b464e8e0f3343b3b
KiriHoy <kiri.hoy@digital.gov.au>
2019-10-28 11:00:10 +1100

    set aria label attribute

--- packages/pagination/src/js/react.js
+++ packages/pagination/src/js/react.js
@@ -14,42 +14,73 @@ import PropTypes from 'prop-types';
 /**
  * AUpagination
  * @param  {object[]}   items 				- items in pagination list
- * @param  {object} 	pageLimit      		- Number of records to be shown per page 
- *  @param {object} 	currentPage      	- 
+ * @param {object} 	currentPage      	- 
  * @param  {object}   	attributeOptions   	- Default HTML attributes
  */
 
 const AUPagination = ({ items, className, ...attributeOptions}) => {
+
 	return (
-<ul className={ ` au-link-list au-link-list--inline ${ className }`} { ...attributeOptions }>
-	<AUPaginationItem>Previous</AUPaginationItem>
-		{
-			items.map(
-				( item, i ) => <AUPaginationItem  key={ i } >{ item }</AUPaginationItem>
-			)
-		}
-	<AUPaginationItem>Next</AUPaginationItem>
-	</ul>
+			<nav role="navigation" aria-label="Pagination Navigation" className={`au-pagination ` + `${className}`} {...attributeOptions}>
+				<ul className={ ` au-link-list au-link-list--inline ${ className }`} { ...attributeOptions }>
+					<AUPaginationItem>Previous</AUPaginationItem>
+						{
+							items.map(
+								( item, i ) => <AUPaginationItem  key={ i } >{ item }</AUPaginationItem>
+							)
+						}
+					<AUPaginationItem>Next</AUPaginationItem>
+					</ul>
+			</nav>
 	)
 };
 
 AUPagination.propTypes = {
-	dark: PropTypes.bool
+	dark: PropTypes.bool,
+	className: PropTypes.string
 };
 
-AUPagination.defaultProps = {};
+AUPagination.defaultProps = {
+	className: ''
+
+};
 
 /**
  * The pagination item component
  * @param {string} className        - An additional class, optional
  * @param {object} attributeOptions - Default HTML attributes
+ * @param {string} label			- aria-label for pagination items
  */
-export const AUPaginationItem = ( { children, className,...attributeOptions } ) => {
-	return <li className={` ${ className }`} { ...attributeOptions }>
-				<a>
+export const AUPaginationItem = ( { children, className, label, ...attributeOptions } ) => {
+	
+	if ( children === 'Previous' ) {
+		label = "go to previous page";
+		 
+	}
+	else if (children === 'Next') {
+
+		label = "go to next page";
+
+	}
+	else {
+		label = "Page " + children;
+	}
+	
+
+	return <li className={`au-pagination__item ${ className }`} { ...attributeOptions }>
+				<a href="#0" className={`au-pagination__link ${ className }`} aria-label={ label }>
 					{children}
 				</a>
 			</li>
+};
+
+AUPaginationItem.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string
+}
+
+AUPaginationItem.defaultProps = {
+	className: ''
 };
 
 export default AUPagination;
