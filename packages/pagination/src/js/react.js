b4dd1d9d4ceba84d41b5e223507288f65c2e8180
KiriHoy <kiri.hoy@digital.gov.au>
2019-11-04 15:37:53 +1100

    adding disabled and onClick events to pagination

--- packages/pagination/src/js/react.js
+++ packages/pagination/src/js/react.js
@@ -7,7 +7,7 @@
  *
  **************************************************************************************************************************************************************/
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const LEFT_PAGE = '...';
@@ -16,19 +16,18 @@ const NEXT = 'Next';
 const PREVIOUS = 'Previous';
 
 //Helper method for creating array of numbers for pagination
-const createPaginationarray = (from, to) => {
-  let i = from;
+const createPaginationarray = (first, last) => {
+  let i = first;
   const PaginationItems = [];
 
-  while (i <= to) {
+  while (i <= last) {
+
     PaginationItems.push(i);
     i += 1;
   }
 
   return PaginationItems;
 }
-
-
 
 class AUPagination extends React.Component {
 
@@ -39,10 +38,12 @@ class AUPagination extends React.Component {
 
 	constructor( props ) {
 	  super( props );
-	  const { totalResults, recordsPerPage, totalPaginationItems, className = ''} = props;
+	  const { totalResults, recordsPerPage, totalPaginationItems, className = '' , isDisabled} = props;
   
-	  this.fetchPaginationItems = this. fetchPaginationItems.bind(this);
-
+	  this.fetchPaginationItems = this.fetchPaginationItems.bind(this);
+	  this.handleClick = this.handleClick.bind(this);
+	  this.handleNextClick = this.handleClick.bind(this);
+	  
 	  this.recordsPerPage = typeof recordsPerPage === 'number' ? recordsPerPage : 10;
 	  this.totalResults = typeof totalResults === 'number' ? totalResults : 0;
 
@@ -51,19 +52,70 @@ class AUPagination extends React.Component {
 	  this.state = { currentPage: 1 };
 	}
 
-	fetchPaginationItems() {
+	
 
+	fetchPaginationItems() {
 		const totalPaginationItems = this.totalPaginationItems;
 		const currentPage = this.state.currentPage;
+		const totalNumbers = 5;
+    	const totalBlocks = 7;
 
-		const startPage = Math.max(2, currentPage);
-		const endPage = Math.min(totalPaginationItems - 1, currentPage);
-  
-		let pages = (startPage, endPage);
-		
+    if (totalPaginationItems > totalBlocks) {
+
+      const startPage = Math.max(2, currentPage - 2);
+      const endPage = Math.min(totalPaginationItems - 1, currentPage + 2);
+      let pages = createPaginationarray(startPage, endPage);
+      const extraResultsLeft = startPage > 2;
+      const extraResultsRight = (totalPaginationItems - endPage) > 1;
+      const spillOffset = totalNumbers - (pages.length + 1);
+
+      switch (true) {
+       
+        case (extraResultsLeft && !extraResultsRight): {
+          const extraPages = createPaginationarray(startPage - spillOffset, startPage - 1);
+          pages = [LEFT_PAGE, ...extraPages, ...pages];
+          break;
+        }
+
+        
+        case (!extraResultsLeft && extraResultsRight): {
+          const extraPages = createPaginationarray(endPage + 1, endPage + spillOffset);
+          pages = [...pages, ...extraPages, RIGHT_PAGE];
+          break;
+        }
+
+       
+        case (extraResultsLeft && extraResultsRight):
+        default: {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+          break;
+        }
+      }
+
+      return [1, ...pages, totalPaginationItems];
+
+    }
+
 		return createPaginationarray(1, totalPaginationItems) ;
-	
 	  }
+
+	 handleClick() {
+		let listid = Number(event.target.id);
+		const currentPage = listid;
+        this.setState({
+		  currentPage
+		});
+		
+      }
+
+	  handleNextClick() {
+		let listid = Number(event.target.id);
+		const currentPage = listid + 1;
+        this.setState({
+		  currentPage
+		});
+		
+      }
 
 	render() {
 
@@ -73,19 +125,19 @@ class AUPagination extends React.Component {
 		const items = this.fetchPaginationItems();
 	
 		return (
-		 
+		
 			<nav role="navigation" aria-label="Pagination Navigation" className={`au-pagination ` + `${className} `}>
 			<ul className={ ` au-link-list au-link-list--inline ${ className } ` }>
-			<AUPaginationItem className={` ${currentPage === 1 ? 'disabled' : ''} `}>Next</AUPaginationItem>	
+			<AUPaginationItem className ={ `${currentPage === 1? 'disabled' : ''}`} onClick={ this.handleNextClick}>Next</AUPaginationItem>	
 				{ items.map(( item, i ) => {
 						
 				  return (
-					<AUPaginationItem key={ i }>{ item }</AUPaginationItem>			
+					<AUPaginationItem  className ={ `${currentPage === item ? 'active' : ''}`} key={ i } onClick={ this.handleClick}  id={item}  >{ item }</AUPaginationItem>			
 				  );
 	
 				}) }
-			<AUPaginationItem className={` ${currentPage === totalPaginationItems ? 'disabled' : ''} `}>Previous</AUPaginationItem>	
-	
+			<AUPaginationItem>Previous</AUPaginationItem>	
+		
 			  </ul>
 			</nav>
 			
@@ -99,7 +151,8 @@ class AUPagination extends React.Component {
   AUPagination.propTypes = {
 	totalResults: PropTypes.number.isRequired,
 	recordsPerPage: PropTypes.number,
-	 onChanged: PropTypes.func
+	onClick: PropTypes.func,
+	onPageChanged: PropTypes.func
   };
 
   AUPagination.defaultProps = {
@@ -114,10 +167,9 @@ class AUPagination extends React.Component {
  * @param { string } className        - An additional class, optional
  * @param { object } attributeOptions - Default HTML attributes
  * @param { string } label			  - aria-label for pagination items
- * @param { bool } 	 disabled		  - aria-label for pagination items
  * @param  {object}  onClick          - The onChange event handler
  */
-export const AUPaginationItem = ( { children, className, label, onClick, ...attributeOptions } ) => {
+export const AUPaginationItem = ( { children, className, label, ...attributeOptions } ) => {
 	
 	// set aria label attribute
 	if ( children === 'Previous' ) {
@@ -135,7 +187,7 @@ export const AUPaginationItem = ( { children, className, label, onClick, ...attr
 	
 
 	return <li className={`au-pagination__item ${ className }`}>
-			<a href="#0" className={`au-pagination__link ${ className }`}  aria-label={label}>
+			<a href="#0" className={`au-pagination__link ${ className }`}  { ...attributeOptions }  aria-label={label}>
 				{children}
 			</a>
 			</li>
